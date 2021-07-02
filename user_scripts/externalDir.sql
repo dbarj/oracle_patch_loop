c9c5e48b7ee486599622411cd39f2a362eea5db4
Rodrigo Jorge <rodrigoaraujorge@hotmail.com>
2021-07-02 14:31:59 -0300

    Last fixes

--- user_scripts/externalDir.sql
+++ user_scripts/externalDir.sql
@@ -3,4 +3,14 @@ WHENEVER SQLERROR EXIT SQL.SQLCODE
 
 CREATE OR REPLACE DIRECTORY expdir AS '&1';
 
+col v_username new_v v_username nopri
+
+select case when version >= 12 then 'C##' end || 'HASH' v_username
+from  (select to_number(substr(version,1,instr(version,'.')-1)) version
+         from v$instance);
+
+col v_username clear
+
+GRANT READ,WRITE ON DIRECTORY expdir to &v_username.;
+
 EXIT 0
