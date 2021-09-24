8a2f4ec25050bf11024491cef7a1194e7f643e9a
Rodrigo Jorge <rodrigoaraujorge@hotmail.com>
2021-09-23 21:20:07 -0300

    Add file_type in T_FILES

--- user_scripts/odbfcl/adb_load_filechksum/load_chksum_table.sql
+++ user_scripts/odbfcl/adb_load_filechksum/load_chksum_table.sql
@@ -1,8 +1,9 @@
 WHENEVER SQLERROR EXIT SQL.SQLCODE
 
-insert /*+ append */ into T_FILES (PATH, SHA256_HASH, ORAVERSION, ORASERIES, ORAPATCH)
+insert /*+ append */ into T_FILES (PATH, SHA256_HASH, FILE_TYPE, ORAVERSION, ORASERIES, ORAPATCH)
 select path,
        hash,
+       file_type,
        substr(file_name,instr(file_name,'_',1,1)+1,instr(file_name,'_',1,2)-instr(file_name,'_',1,1)-1)           oraversion,
        substr(file_name,instr(file_name,'_',1,2)+1,instr(file_name,'_',1,3)-instr(file_name,'_',1,2)-1)           oraseries,
        to_number(substr(file_name,instr(file_name,'_',1,3)+1,instr(file_name,'.',-1)-instr(file_name,'_',1,3)-1)) orapatch
