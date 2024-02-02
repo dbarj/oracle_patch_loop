cafb376325c976983f3cf22ac94bd473fa56cc99
Rodrigo Jorge <rodrigoaraujorge@hotmail.com>
2024-02-02 15:38:52 -0300

    Extract for 11.2.0.1 and 11.2.0.2

---
+++ user_scripts/odbfcl/extract/underscore_parameters/load_underscore_11.2.0.2.sql
@@ -0,0 +1,6 @@
+--------------------------------------
+-- 11.2.0.2
+--------------------------------------
+
+-- The code is the same as 11.2.0.4 in V$FIXED_VIEW_DEFINITION
+@@load_underscore_11.2.0.4.sql
---
+++ user_scripts/odbfcl/extract/underscore_valid_values/load_underscore_vv_11.2.0.1.sql
@@ -0,0 +1,6 @@
+--------------------------------------
+-- 11.2.0.1
+--------------------------------------
+
+-- The code is the same as 11.2.0.4 in V$FIXED_VIEW_DEFINITION
+@@load_underscore_vv_11.2.0.4.sql
